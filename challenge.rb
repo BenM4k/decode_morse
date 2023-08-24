d6fc11c74468a8724428fea58165b242f471dbc1
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-08-24 11:51:02 +0200

    fix linters errors

--- challenge.rb
+++ challenge.rb
@@ -1,32 +1,31 @@
-$morse_code = {
+def first_method(string)
+  morse_code = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
     '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
     '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
     '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
-}
-
-def first_method(string)
-    string = string.strip
-    morse_char = $morse_code[string] || ''
-    morse_char
+  }
+  string = string.strip
+  morse_char = morse_code[string] || ''
+  morse_char
 end
 
 def second_method(string)
-    morse_chars = string.split(' ').map { |morse_word| first_method(morse_word) || ' '}.join
-    morse_chars
+  morse_chars = string.split(' ').map { |morse_word| first_method(morse_word) || ' ' }.join
+  morse_chars
 end
 
 def morse(string)
-    morse_chars = string.split('  ')
-    decoded_words = morse_chars.map do |morse_char|
-        decoded_word = second_method(morse_char)
-        decoded_word
-    end
+  morse_chars = string.split('  ')
+  decoded_words = morse_chars.map do |morse_char|
+    decoded_word = second_method(morse_char)
+    decoded_word
+  end
 
-    decoded_phrase = decoded_words.join(' ')
-    decoded_phrase
+  decoded_phrase = decoded_words.join(' ')
+  decoded_phrase
 end
 
 input = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
